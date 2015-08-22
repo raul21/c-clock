6cdd1fdf5632401dff40feb6bf5f0e78ca1c0e67
raul21 <raulsauber@gmail.com>
2015-08-22 19:19:26 +0000

    Stub functions - partial

---
+++ clock_v2.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main () {
+	return 0;
+}
+
+/**
+ * Receives the timestamp and returns the total number of seconds  
+ *
+ */
+int get_seconds (int time){
+	return 0;
+}
+
+/**
+ * Receives the timestamp and returns the total amount of minutes
+ *
+ */
+int get_minutes (int time){
+	return 0;
+}
+
+/**
+ * Receives the timestamp and returns the total number of the hours
+ *
+ */
+int get_hours (int time){
+	return 0
+}
+
+/**
+ * Receives the timestamp and returns the total number of days 
+ *
+ */
+int get_days (int time){
+	return 0;
+}
+
+/**
+ * Receives the timestamp and returns the number of passed years
+ *
+ */
+int get_years (int time){
+	return 0;
+}
+
+/**
+ * Receives he timestamp and returns the current year
+ *
+ */
+int get_current_year (time){
+	return 0;
+	
+}
+
+/**
+ * Receives the timestamp and returns the current month
+ *
+ */
+int get_current_month (time){
+	return 0;
+}
+
+/**
+ * Receives the timestamp and returns the number of bisect years
+ *
+ */
+int get_bisect_years_number (time){
+	return 0;
+}
